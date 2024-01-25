1a67eefce151d3933b7f7c72b64f0a0b125dfea5
Apoorv-2711 <apoorvjain162@gmail.com>
2024-01-25 20:24:03 +0530

    PWA feature added

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,15 @@ import "./globals.css";
 
 export const metadata = {
   title: "Dank Chat",
-  description: "Chat room made-by: Apoorv Jain"
-
+  description: "Chat room made-by: Apoorv Jain",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["DankGC", "Group Chat App", "Group fun", "Chatting", "application"],
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" >
+    <html lang="en">
       <body className="text-black bg-white dark:text-white dark:bg-[#1c1c1c] transition-colors duration-300">
         <Navigator>{children}</Navigator>
       </body>
--- next.config.js
+++ next.config.js
@@ -6,4 +6,10 @@ const nextConfig = {
       },
 }
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+module.exports = withPWA(nextConfig);
